c10f16cf61661dc924b687469a22e45080c12311
sombrastudios <nils.martel@concepts-system.com>
2018-05-06 18:39:38 +0200

    Implement Laplace Algorithm to calculate Determinante

--- src/matrix.rs
+++ src/matrix.rs
@@ -17,9 +17,9 @@ impl Matrix {
         }
     }
 
-    pub fn rows() -> i32 { self.rows }
+    pub fn get_rows(&self) -> i32 { self.rows }
 
-    pub fn cols() -> i32 { self.cols }
+    pub fn get_cols(&self) -> i32 { self.cols }
     
     pub fn get_value(&self, row: i32, col: i32) -> f64 {
         if ! self.coordinate_in_matrix(row, col) {
@@ -54,4 +54,18 @@ impl Matrix {
     fn coordinate_in_matrix(&self, row: i32, col: i32) -> bool {
         row >= 0 && row < self.rows && col >= 0 && col < self.cols
     }
+}
+
+
+fn determinante_laplace(m: &Matrix) -> f64 {
+    if m.get_cols() == 2 && m.get_rows() == 2 {
+       return m.get_value(0, 0) * m.get_value(1, 1) - m.get_value(0,1) * m.get_value(1, 0);
+    }
+    let mut det = 0_f64;
+    for i in 0..m.cols {
+        if i != 0 {
+            det += i as f64 * determinante_laplace(m) * if i | 1 == 1 { -1.0 } else { 1.0 };
+        }
+    }
+    det
 }
